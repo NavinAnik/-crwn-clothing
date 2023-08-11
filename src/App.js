5b51eeb6f52b7afde4e344b448b3b1203b52e211
Anik <66571431+NavinAnik@users.noreply.github.com>
2023-08-11 15:50:22 +0600

    converted data into jsx UI

--- src/App.js
+++ src/App.js
@@ -2,23 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const categories = [
+        {
+            id : 1,
+            tittle : 'HATS',
+        },
+        {
+            id : 2,
+            tittle : 'JACKETS',
+        },
+        {
+            id : 1,
+            tittle : 'SNEAKERS',
+        },
+        {
+            id : 1,
+            tittle : 'WOMENS',
+        },
+        {
+            id : 1,
+            tittle : 'MENS',
+        },
+    ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="categories-contrainer">
+        {categories.map(({tittle }) =>(
+            <div className= "categories-contrainer">
+                <div className= 'background-image' />
+                <div className="category-body-container">
+                    <h2>{tittle}</h2>
+                    <p>shop now</p>
+                </div>
+            </div>
+        ))}
     </div>
+
+
   );
 }
 
